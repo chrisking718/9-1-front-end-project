069cc4d23fb3ada3c53f6fcb47de1b6bc10cbe5f
chrisking718 <chrisking@pursuit.org>
2022-09-09 21:46:59 -0400

    draws cards, removes aces from deck, alows user to start new game, insert draw button for both players

--- main.js
+++ main.js
@@ -1,23 +1,4 @@
-//const URl = 'https://deckofcardsapi.com/api/deck/new/draw/?count=2'
-//url to get and shuffle deck
-const URl = "http://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
-
-// deckID will be changed after the deck is shuffled
-let deckID = ""
-
-// draw first cards using the deck id
-const firstDraw = `https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=4`
-
-// link to add card to player 1 pile
-const player1Pile = `http://deckofcardsapi.com/api/deck/${deckID}/pile/player1/add/?cards=AS,2S `
-
-// link to add card to player 2 pile
-const Player2Pile = `http://deckofcardsapi.com/api/deck/${deckID}/pile/player2/add/?cards=AS,2S `
-
-// link to shuffle when user wants to start new game with same deck . 
-const shuffle = `http://deckofcardsapi.com/api/deck/${deckID}/shuffle/`
-
-/*
+/*Proposal #1
    - The initial URl for the first fetch contains "new" to get new deck ID
    - Once I have the deck ID, I have to store so that the cards come from the same deck until a new game starts
    - the "new" will now be replaced by that deck ID
@@ -31,18 +12,128 @@ const shuffle = `http://deckofcardsapi.com/api/deck/${deckID}/shuffle/`
    - Create alert that if an ACE is drawn the player can choose the number to be 1 or 11
    - If any player total exceeds 21. create and display alert of "BUST"
 
-   
    */
 
-fetch(URl)
-    .then((res)=> res.json())
-    .then((resJson)=> {console.log(resJson)
-    deckID = resJson["deck_id"]
-    console.log(deckID)
+/*Proposoal #2
+- Highest Pair without Aces
+- The initial URl for the first fetch contains "new" to get new deck ID
+   - Once I have the deck ID, I have to store the ID so that the cards come from the same deck until a new game starts
+   - the "new" will now be replaced by that deck ID
+   - Add "new game" button and event listener to fetch the deck.
+   - Add AS AD AH AC to junk pile.
+   - Add sections for player one and player two cards.
+   - Add deal button with event listener and NEW fetch with the deckID to get cards from new deck and deal to each player.
+    - When cards are dealt, the first card is face down, but the second card is face up.
+    - Set hidden for front and back images with toggle. 
+   - The value of each card dealt to each player should be stored in a value for each player.
+   - Declare that if the value is QUEEN KING or JACK - the number added is 10
+
+*/
+
+//const URl = 'https://deckofcardsapi.com/api/deck/new/draw/?count=2'
+
+//url to get and shuffle deck
+const URl = "http://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
+
+// deckID will be changed after the deck is shuffled
+let deckID = ""
+
+const BASE = "https://deckofcardsapi.com/api/deck/"
+// draw first cards using the deck id
+const firstDraw = `/draw/?count=4`
+
+// link to add card to player 1 pile
+const player1Pile = `/pile/player1/add/?cards=AS,2S `
+
+// link to add card to player 2 pile
+const Player2Pile = `/pile/player2/add/?cards=AS,2S `
+
+// link to shuffle when user wants to start new game with same deck . 
+const shuffle = `/shuffle/`
+
+//add all aces to a junk pile.
+const junkPile = `/pile/junk/add/?cards=AS,AD,AC,AH `
+
+const player1 = document.querySelector(".player1")
+const player2 = document.querySelector(".player2")
+
+const newGame = document.querySelector("#newGame")
+
+let drawnImages = []
+let drawnValues = []
+// when player clicks new new game button. game starts
+
+let tempDeck = "qr98ugy0plse"
+
+newGame.addEventListener(("submit"), (event)=>{
+    event.preventDefault()
+// console.log(`${BASE}${deckID}${firstDraw}`)
+
+// this fetch adds aces to junk pile
+fetch(`${BASE}${tempDeck}${shuffle}`)
+    
+    .then((res1) => res1.json())
+    .then((res1) => {
+        console.log(res1)
+
+        //this fetch draws the first 4 cards in the new game
+        fetch(`${BASE}${tempDeck}${firstDraw}`)
+            .then((res2) => res2.json())
+            .then((res2) => {
+                 console.log(res2)
+
+                 console.log(res2.cards.length)
+
+        // stores value and images for each card drawn   
+                for(let i = 0;i<res2.cards.length;i++){
+                            drawnImages.push(res2.cards[i].image)
+                            drawnValues.push(res2.cards[i].value)
+                        }
+                        console.log("drawn:" + drawnValues)
+
+        //create draw player 1 button      
+            const draw1 = document.createElement('form')
+            const draw1butt = document.createElement('input')
+        
+            draw1butt.setAttribute("type","submit")
+            draw1butt.setAttribute("value","Draw Player 1")
+            
+        //create draw player two button
+            const draw2 = document.createElement('form')
+            const draw2butt = document.createElement('input')
+
+            draw2butt.setAttribute("type","submit")
+            draw2butt.setAttribute("value","Draw Player 2")
+            
+
+            draw1.append(draw1butt)
+            draw2.append(draw2butt)
+            player1.appendChild(draw1)
+            player2.appendChild(draw2)
+            })
+        
+            .catch((error)=>console.log(error))
+
     })
+    .catch((error)=>console.log(error))
+
+    
+})
 
 
-    .catch((error)=> console.log(error))
+
+
+
+// get new eck and Deck ID, changes deck ID on global scope
+// fetch(URl)
+//     .then((res)=> res.json())
+//     .then((resJson)=> {console.log(resJson)
+//     deckID = resJson["deck_id"]
+//     //console.log(deckID)
+//     })
+
+
+//     .catch((error)=> console.log(error))
 
 
 
